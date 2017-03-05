de7c51575f7637d2b2b3ae30df17af408a9d792c
Ashot Arutyunyan <jcl9595@gmail.com>
2017-03-05 17:25:59 -0500

    added click buttons and established many_to_many relationship

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,5 @@
 class Boat < ApplicationRecord
+	has_many :jobboats
+	has_many :jobs, through: :jobboats
 	validates :name, uniqueness: true
 end
---
+++ app/models/jobboat.rb
@@ -0,0 +1,4 @@
+class Jobboat < ApplicationRecord
+	belongs_to :job
+	belongs_to :boat
+end
