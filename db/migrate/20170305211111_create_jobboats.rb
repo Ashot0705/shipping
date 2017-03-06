ab8fed0120a5890b4e48244bb94fd47e036808e7
ngoan230 <ngoan_230@yahoo.com>
2017-03-06 13:42:16 -0500

    Revert "An2"

--- db/migrate/20170305211111_create_jobboats.rb
+++
@@ -1,8 +0,0 @@
-class CreateJobboats < ActiveRecord::Migration[5.0]
-  def change
-    create_table :jobboats do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20170305211247_add_column_to_jobboat.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToJobboat < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :jobboats, :job_id, :integer
-  	add_column :jobboats, :boat_id, :integer
-  end
-end
